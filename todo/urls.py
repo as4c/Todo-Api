b1243fc20eeb8db5e27dfb1358d659984dc403b9
as4c <sagarkumarsameer@gmail.com>
2023-06-01 15:30:00 +0530

    deployment config

--- todo/urls.py
+++ todo/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,9 @@ urlpatterns = [
     path('users/',include('users.urls')),
     path('api-auth/', include('rest_framework.urls'))
 ]
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()

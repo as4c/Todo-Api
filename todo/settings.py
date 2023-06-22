c44a636f99721dc81649f23a6d127c434c6f58b8
as4c <sagarkumarsameer@gmail.com>
2023-06-22 18:11:36 +0530

    final commit

--- todo/settings.py
+++ todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-is^b4=5lxn1v6-&9-vp238*(pj98+)vk-_d*(r5gzq80_p62c+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -155,4 +155,10 @@ REST_FRAMEWORK = {
     ),
 }
 
+CORS_ALLOWED_ORIGINS = [
+    "http://as4c.pythonanywhere.com",
+    "https://api.postman.com",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
 
+]

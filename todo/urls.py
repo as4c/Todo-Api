6aae646dac8083e1cc2a5e2c725c536bfa702227
sagar kumar <84590258+as4c@users.noreply.github.com>
2023-10-26 16:17:41 +0530

    Update urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -19,14 +19,14 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from api import views
+
 
 
 
 
 
 urlpatterns = [
-    path('',views.homepage,name='home'),
+    
     path('admin/', admin.site.urls),
     path('api/',include('api.urls')),    # urls for my todo api
     path('users/',include('users.urls')), # urls for users accounts
